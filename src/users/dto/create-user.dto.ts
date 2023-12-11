62bac98d8070b6ae12dde3c1bfcd990f8c060efb
Lúcio Henrique Alves Silveira <lucions96@gmail.com>
2023-12-11 20:38:08 -0300

    wip - create users

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,8 @@
+export class CreateUserDto {
+  fullName: string;
+  email: string;
+  birthDate: string;
+  phoneNumber: string;
+  registrationNumber: string;
+  cpf: string;
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,8 @@
+export class User {
+  fullName: string;
+  email: string;
+  birthDate: string;
+  phoneNumber: string;
+  registrationNumber: string;
+  cpf: string;
+}
